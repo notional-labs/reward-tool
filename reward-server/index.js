9704346b04382257086d44a778c3823d3a176e55
Son Trinh <sontrinh@Sons-MacBook-Pro.local>
2023-01-19 04:44:53 +0700

    add graphql

--- reward-server/index.js
+++ reward-server/index.js
@@ -1,19 +1,70 @@
 const express = require('express')
 const app = express()
-const fs = require('fs');
+const axios = require('axios')
+
+require('dotenv').config()
+
+const graphqlReq = axios.create({
+    baseURL: "https://graphql.fauna.com/graphql",
+    headers: {
+        Authorization: `Bearer ${process.env.FAUNADB_SECRET}`,
+    },
+});
+
+const getRecords = async () => {
+    const res = await graphqlReq({
+        method: "POST",
+        data: {
+            query: `
+                query{
+                    getRecords {
+                        data {
+                            date,
+                            rewards,
+                            total
+                        }
+                    }
+                }
+              `
+        },
+    })
+    return res.data
+}
 
 const port = 3002
 
-app.get('/', (_, res) => {
-    fs.readFile('../reward-bot/data.json', (err, data) => {
-        if (err) {
+const formatData = (list) => {
+    let formatList = []
+    list.forEach(e => {
+        formatList.unshift({
+            ...e,
+            rewards: JSON.parse(e.rewards)
+        })
+    });
+
+    return formatList
+}
+
+app.get('/', async (_, res) => {
+    try{
+        const { data } = await getRecords()
+        const rewards = data !== null && data.getRecords && data.getRecords !== null && data.getRecords.data
+        if (rewards !== null) {
             res.json({
-                err: 'fail to fetch data: ' + err.message
+                data: formatData(rewards)
             })
         }
-        const result = JSON.parse(data)
-        res.json(result)
-    });
+        else {
+            res.status(404).send({
+                err: "data not found"
+            })
+        }
+    }
+    catch (e) {
+        res.status(400).send({
+            err: e.message
+        })
+    }
 })
 
 app.listen(port, async () => {
