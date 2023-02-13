c7847ff8bddc50e809f524794755780935491ecb
Son Trinh <sontrinh@Sons-MacBook-Pro.local>
2023-02-13 14:52:18 +0700

    add token change

--- reward-server/index.js
+++ reward-server/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const axios = require('axios')
+const { denomToId } = require('./data')
 
 require('dotenv').config()
 
@@ -10,6 +11,8 @@ const graphqlReq = axios.create({
         Authorization: `Bearer ${process.env.FAUNADB_SECRET}`,
     },
 });
+
+var queryString = 'https://api.coingecko.com/api/v3/simple/price?include_24hr_change=false&vs_currencies=usd&ids=agoric,stride-staked-atom,akash-network,darc,omniflix-network,konstellation,axelar,usd-coin,tether,bostrom,dai,wei,matic-network,avalanche-2,polkadot,band-protocol,bzedge,bitcanna,bitsong,switcheo,cerberus-2,cheqd-network,chihuahua-token,comdex,cosmos,crescent-network,crypto-com-chain,decentr,desmos,dig-chain,echelon,e-money,evmos,fetch-ai,injective-protocol,iris-network,ixo,jackal,juno-network,kava,ki,kujira,lambda,likecoin,lum-network,nym,odin-protocol,geodb,osmosis,ion,somm,persistence,point-network,provenance-blockchain,rebus,regen,rizon,secret,sentinel,certik,sifchain,stafi,stafi-ratom,stargaze,starname,stride,teritori,terra-luna,terrausd,terrakrw,white-whale,terra-luna-2,umee,unification,vidulum'
 
 const getRecords = async () => {
     const res = await graphqlReq({
@@ -34,18 +37,27 @@ const getRecords = async () => {
 
 const port = 3002
 
-const formatData = (list) => {
+const formatData = async (list) => {
     let formatList = []
     if (list.length >= 2) {
-        for(let i = 0; i< list.length; i++) {
+        for (let i = 0; i < list.length; i++) {
             let change_in_24h = 0
+            let token_gain_in_24h = 0
+            let token_change_in_24h 
             if (i >= 1) {
-                change_in_24h = list[i].total_asset - list[i-1].total_asset
+                change_in_24h = list[i].total_asset - list[i - 1].total_asset
+                const { changeList, changeSum } = await getRewardsDiff(JSON.parse(list[i].rewards), JSON.parse(list[i - 1].rewards))
+                token_change_in_24h = changeList
+                token_gain_in_24h = changeSum
             }
             formatList.unshift({
-                ...list[i],
+                date: list[i].date,
                 rewards: JSON.parse(list[i].rewards),
-                change_in_24h: change_in_24h
+                list_token_change_in_24h: token_change_in_24h,
+                'token_gain_in_24h (usd)': token_gain_in_24h,
+                'available_asset (usd)': list[i].available_asset,
+                'total_asset (usd)': list[i].total_asset,
+                'asset_change_in_24h (usd)': change_in_24h,
             })
         }
     }
@@ -60,13 +72,63 @@ const formatData = (list) => {
     return formatList
 }
 
+const getRewardsDiff = async (reward_1, reward_2) => {
+    let changeList = {}
+    let changeSum = 0
+    const tokenList_1 = getTotal(reward_1)
+    const tokenList_2 = getTotal(reward_2)
+    const res = await axios.get(queryString)
+    const usdRates = res.data
+    for (let key in tokenList_1) {
+        let tokenChangeList = {}
+        for (let k in tokenList_1[key]) {
+            const id = denomToId[k]
+            tokenChangeList[k] = {}
+            const rate = usdRates[id] ? (usdRates[id].usd && usdRates[id].usd.value) ? 0 : usdRates[id].usd || 0 : 0
+            const diff = tokenList_1[key][k] - tokenList_2[key][k]
+            tokenChangeList[k].amount = diff
+            tokenChangeList[k].usd = (diff * rate).toFixed(2)
+            changeSum += diff * rate
+        }
+        changeList[key] = tokenChangeList
+    }
+    return { changeList, changeSum }
+}
+
+const getTotal = (reward) => {
+    let tokenList = {}
+    for (const index in reward) {
+        let denomList = {}
+        reward[index].total.reward && reward[index].total.reward.length > 0 && reward[index].total.reward.map(re => {
+            if (!denomList[re.denom] || denomList[re.denom] === null) {
+                denomList[re.denom] = 0
+            }
+            denomList[re.denom] += parseFloat(re.amount)
+        })
+        reward[index].total.commission && reward[index].total.commission.length > 0 && reward[index].total.commission.map(com => {
+            if (!denomList[com.denom] || denomList[com.denom] === null) {
+                denomList[com.denom] = 0
+            }
+            denomList[com.denom] += parseFloat(com.amount)
+        })
+        reward[index].total.stake_amount && reward[index].total.stake_amount.length > 0 && reward[index].total.stake_amount.map(stake => {
+            if (!denomList[stake.denom] || denomList[stake.denom] === null) {
+                denomList[stake.denom] = 0
+            }
+            denomList[stake.denom] += parseFloat(stake.amount)
+        })
+        tokenList[index] = denomList
+    }
+    return tokenList
+}
+
 app.get('/', async (_, res) => {
-    try{
+    try {
         const { data } = await getRecords()
         const rewards = data !== null && data.getRecords && data.getRecords !== null && data.getRecords.data
         if (rewards !== null) {
             res.json({
-                data: formatData(rewards)
+                data: await formatData(rewards)
             })
         }
         else {
